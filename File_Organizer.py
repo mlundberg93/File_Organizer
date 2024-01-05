caeac53680b2b777d5cbda5d6b2672029df08534
Marcus <145341135+mlundberg93@users.noreply.github.com>
2024-01-05 12:48:33 +0100

    Tweaked the layout a bit

--- File_Organizer.py
+++ File_Organizer.py
@@ -4,42 +4,44 @@ sg.theme("Reds")
 left_side = [
     [
         sg.Text("Left folder"),
-        sg.Input(size=(20,5), key="-Left Folder-"),
-        sg.FolderBrowse()
+        sg.In(size = (25,1), enable_events = True, key = "-Left Folder-"),
+        sg.FolderBrowse(),
     ],
-    [
-        sg.Text("Folder List"),
-        sg.Listbox(values=[], size=(40,20), key="-Left List-")
+    [       
+        sg.Listbox(values = [], enable_events = True, size = (40,20), key = "-Left List-")
     ]
 ]
 right_side = [
-        [sg.Text("Right Folder"),
-        sg.Input(size=(20,5), key="-Right Folder-"),
-        sg.FolderBrowse()
+    [
+        sg.Text("Right Folder"),
+        sg.In(size=(25,1), enable_events=True, key="-Right Folder-"),
+        sg.FolderBrowse(),
     ],
     [
-        sg.Text("Folder List"),
-        sg.Listbox(values=[], size=(40,20), key="-Right List-")
+        sg.Listbox(values=[], enable_events=True, size=(40,20), key="-Right List-")
     ]
 ]
-bottom_menu = [
-    [    
-        sg.Button("Move"), 
-        sg.Button("Copy"), 
-        sg.Button("Create folder"), 
-        sg.Exit(button_color="tomato", s=15)
-    ]
+button_size = (10,3)
+middle_menu = [
+        [
+            sg.Button("-->", enable_events = True, key = "-Swap-", size = button_size)
+        ],
+        [
+            sg.Button("Move", enable_events = True, key = "-Move-", size = button_size),
+        ], 
+        [
+            sg.Button("Copy", enable_events = True, key = "-Copy-", size = button_size),
+        ], 
+        [
+            sg.Button("Create folder", enable_events = True, key = "-Create-", size = button_size),
+        ], 
+        [
+            sg.Exit(button_color="tomato", size = (10,5))
+        ]  
 ]
-layout = [
-    [
-        sg.Column(left_side),
-        sg.VSeperator(),
-        sg.Column(right_side),
-    ],
-    [sg.HSeparator()],
-    [sg.Column(bottom_menu, justification="r")]
-]
-window = sg.Window("File Organizer", layout, use_custom_titlebar=True)
+
+layout = [[sg.Column(left_side), sg.Column(middle_menu), sg.Column(right_side)]]
+window = sg.Window("File Organizer", layout, finalize = True, use_custom_titlebar = True)
 
 while True:
     event, values = window.read()
